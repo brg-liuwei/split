75151844ce7fa2b3114f55ff1ebb6cb4495a0e74
brg-liuwei <stupidlw@126.com>
2014-06-18 18:07:20 +0800

    leveldb

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+cd lib/leveldb
+make clean
+
+cd ../../
+rm -rf src/kv/testDb
+rm -rf testDb
+rm -rf bin/*
--- go_buid.sh
+++ go_buid.sh
@@ -3,7 +3,11 @@ export GOPATH=$cur_path:$GOPATH
 export CGO_CFLAGS="-I/Users/wliu/Code/split/lib/leveldb/include"
 export CGO_LDFLAGS="-L/Users/wliu/Code/split/lib/leveldb"
 
-cd src
+cd lib/leveldb/
+make
+
+cd ../../
+cd src/
 go test config
 go test util
 go test kv
